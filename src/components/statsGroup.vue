67fa3f53a977dd43547aca766a6b87a8efe7a71b
Manuel <74509303+Manlx@users.noreply.github.com>
2022-12-27 06:57:12 +0200

    Partly done with the stats block

---
+++ src/components/statsGroup.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bigBody">
+        <h1>{{ this.GroupTitle }}</h1>
+        <stats-group-item v-for="item,index in this.DataGroup" :key="index" :displayData="item" :Keys="" :Sizing="" />
+    </div>
+</template>
+
+<script lang="js">
+import statsGroupItem from "./statsGroupItem.vue"
+export default {
+    props:["DataGroup","GroupTitle"],
+    data:()=>{
+        return {}
+    },
+    components:{statsGroupItem}
+}
+</script>
+
+<style scoped>
+    h1{
+        font-size: 1.5rem;
+    }
+
+    .bigBody{
+        width: 100%;
+    }
+</style>
