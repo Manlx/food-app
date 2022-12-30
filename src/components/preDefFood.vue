81b3a05b2654c0ebfe2b01a40914c5d818d9fca3
Manuel <74509303+Manlx@users.noreply.github.com>
2022-12-30 04:23:05 +0200

    Finished MutlSelect Comp and started Dev.

--- src/components/preDefFood.vue
+++ src/components/preDefFood.vue
@@ -1,16 +1,35 @@
 <template>
     <collapse-box :darken="true" :dispTitle="`Show Available Menus`" :noMargin="true" :startOpen="true">
+      <div class="body">
+        <select name="" id="">
+          <option value="">
+            
+          </option>
+        </select>
+        <mutli-select v-if="this.selectedSubParts != []" :arr="this.selectedSubParts" :useSubButton="true"/>
+      </div>
+        
     </collapse-box>    
 </template>
 
 <script>
 import collapseBox from './collapseBox.vue'
+import MutliSelect from './mutliSelect.vue'
 export default {
-  components: { collapseBox },
-
+  components: { collapseBox, MutliSelect },
+  props:["RestaurantLists"],
+  data:function(){
+    return {
+      selectedSubParts:[]
+    }
+  },
+  mounted:function(){
+    
+  }
 }
 </script>
 
 <style scoped>
-
+  .body{
+  }
 </style>
